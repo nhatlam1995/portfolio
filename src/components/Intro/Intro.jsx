a0c2a0480cd37246696c99e3ffdefe671b60af24
nhatlam1995 <43503909+nhatlam1995@users.noreply.github.com>
2022-07-21 05:00:13 +0700

    Intro, Portfolio 50%

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { man, down } from '../../assets'
+import Typewriter from 'typewriter-effect';
 import './styles.scss'
 
 export default function Intro() {
     return (
-        <div className="intro" id='intro'>Intro</div>
+        <div className="intro" id='intro'>
+            <div className="left">
+                <div className="imgContainer">
+                    <img src={man} alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Nguyen Nhat Lam</h1>
+                    <h3>Fresher
+                        <span>
+                            <Typewriter
+                                options={{
+                                    strings: [" React Native", " ReactJS"],
+                                    autoStart: true,
+                                    delay: 70,
+                                    loop: true
+                                }} />
+                        </span>
+                    </h3>
+                </div>
+                <a href="#portfolio">
+                    <img src={down} alt="" />
+                </a>
+            </div>
+        </div>
     )
 }
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,6 +3,18 @@ import './styles.scss'
 
 export default function Portfolio() {
     return (
-        <div className="portfolio" id='portfolio'>Portfolio</div>
+        <div className="portfolio" id='portfolio'>
+            <ul>
+                <li>Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
+        </div>
     )
 }
